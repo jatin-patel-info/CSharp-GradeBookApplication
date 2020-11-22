c99c95a6099422cb64118aec0525fe6b94ed50b3
Jatin Patel <jatin.patel.info@gmail.com>
2020-11-21 19:28:13 -0500

    module 3 changes

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace GradeBook.GradeBooks
@@ -9,6 +10,35 @@ namespace GradeBook.GradeBooks
         public RankedGradeBook(string name): base(name)
         {
             Type = Enums.GradeBookType.Ranked;
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            int studentCount = Students.Count;
+            int gradeSection = studentCount/5;
+
+            if(studentCount < 5)
+            {
+                throw new InvalidOperationException();
+            }
+            //List<Student> OrderedStudents = new List<Student>();
+
+            
+            var orderedStudents = Students.OrderByDescending(s => s.AverageGrade).ToList();
+            var sectionCount = gradeSection;
+
+            if (averageGrade >= orderedStudents[sectionCount - 1].AverageGrade)
+                return 'A';
+            else if (averageGrade >= orderedStudents[sectionCount * 2 - 1].AverageGrade)
+                return 'B';
+            else if (averageGrade >= orderedStudents[sectionCount * 3 - 1].AverageGrade)
+                return 'C';
+            else if (averageGrade >= orderedStudents[sectionCount * 4 - 1].AverageGrade)
+                return 'D';
+            else if (averageGrade >= orderedStudents[sectionCount * 5 - 1].AverageGrade)
+                return 'E';
+            else
+                return 'F';
         }
     }
 }
